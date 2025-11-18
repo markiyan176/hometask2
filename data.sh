c479a4806445bca87ef299679e81107d468773ab
markiyan176 <goro100100@gmail.com>
2025-11-18 05:12:43 +0200

    Initial commit

---
+++ data.sh
@@ -0,0 +1,10 @@
+
+
+
+
+#!/bin/bash
+yum update -y
+yum install -y mc git vim httpd
+echo "<h1>SUCCESS! EC2 setup by AWS CLI and User Data is complete.</h1>" > /var/www/html/index.html
+systemctl start httpd
+systemctl enable httpd
